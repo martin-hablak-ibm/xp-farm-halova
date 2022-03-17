483dd4ef4b19f443de17681429329ec355d0df32
martin-hablak-ibm <martin.hablak@sk.ibm.com>
2022-03-17 17:39:25 +0100

    fix: bootstrap

--- bootstrap/index.js
+++ bootstrap/index.js
@@ -1,6 +1,6 @@
 
 function helloWorld(name) {
-  return "Hello World " + name;
+  return "Hello World "+ name;
 }
 
 module.exports = {helloWorld};
--- bootstrap/index.spec.js
+++ bootstrap/index.spec.js
@@ -2,6 +2,7 @@ const  {helloWorld}  = require("./index");
 
 describe("hello world", () => {
      it("it shold return Hello World martin", () => {
-       helloWorld("martin")
+       res = helloWorld("martin")
+       expect(res).toEqual("Hello World martin")
      });
 });
